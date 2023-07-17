9ab266ba92e693897dc4b0de19e52b8b899a7c40
Horst Dumcke <hdumcke@marlysys.com>
2023-07-17 22:26:41 +0200

    fixing installation scripts

--- webserver/install.sh
+++ webserver/install.sh
@@ -4,9 +4,20 @@ set -x
 ### Get directory where this script is installed
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+source  ~/mini-pupper-release
+
+### Append to release file
+echo WEBCONTROLLER_VERSION=\"$(cd $BASEDIR; ~/mini_pupper_bsp/get-version.sh)\" >> ~/mini-pupper-release
+
 sudo rm -rf /usr/lib/python3/dist-packages/blinker*
-sudo pip install $BASEDIR/backend
-sudo pip install $BASEDIR/../joystick_sim
+if [ "$IS_RELEASE" == "YES" ]
+then
+    sudo PBR_VERSION=$(cd $BASEDIR; ~/mini_pupper_bsp/get-version.sh)  pip install $BASEDIR/backend
+    sudo PBR_VERSION=$(cd $BASEDIR; ~/mini_pupper_bsp/get-version.sh)  pip install $BASEDIR/../joystick_sim
+else:
+    sudo pip install $BASEDIR/backend
+    sudo pip install $BASEDIR/../joystick_sim
+fi
 
 sudo ln -s $BASEDIR/web-controller.service /etc/systemd/system/
 sudo systemctl daemon-reload
